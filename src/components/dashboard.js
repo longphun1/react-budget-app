1ff6becc960f5b8cc71122c22906ac6eb4a3c0f8
Long Phun <longphun90@yahoo.com>
2020-07-21 12:13:25 -0700

    add summary to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './expenseList';
 import ExpenseListFilters from './expenseListFilter';
+import ExpensesSummary from './expensesSummary';
 
 const expenseDashboard = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters/>
         <ExpenseList/>
     </div>
